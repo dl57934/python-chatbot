6720edef3ab9829e84b6973692922f649e7d3fc4
root <dl57934@naver.com>
2018-05-21 08:43:18 +0000

    using markov chain chat bot

--- cgi-bin/botengine.py
+++ cgi-bin/botengine.py
@@ -1,11 +1,12 @@
-import codecs 
 from bs4 import BeautifulSoup
 from konlpy.tag import Twitter
 import os, json, re,random
 import requests 
-dic_file = "./chat-data.json"
+dic_file = "chat-data.json"
 twitter = Twitter()
 dic ={}
+session = requests.session()
+
 def register_dic(words):
     threeDic = ["@"]
     global dic
@@ -48,13 +49,13 @@ def make_sentence(head):
         ret.append(w3)
         if w3 == "." or w3 =="?" or w3 == "" : break
         w1, w2 = w2, w3
-    grammarCheck(ret)
+    ret = "".join(ret)
+    return grammarCheck(ret)
 def word_choice(head):
     li = head.keys()
     return random.choice(list(li))
 
 def grammarCheck(text):
-    session = requests.session()
     header = {
             "accept":"*/*",
             "accept-Language":"ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
@@ -62,9 +63,9 @@ def grammarCheck(text):
             "referer":"https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%EB%84%A4%EC%9D%B4%EB%B2%84+%EB%A7%9E%EC%B6%A4%EB%B2%95+%EA%B2%80%EC%82%AC%EA%B8%B0&oquery=%EB%A7%9E%EC%B6%A4%EB%B2%95+%EA%B2%80%EC%82%AC%EA%B8%B0&tqi=TYFkplpySD0ssbj%2FyLossssstIh-432793"
                 ,"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36"
     }
-    url = " https://m.search.naver.com/p/csearch/ocontent/spellchecker.nhn?_callback=window.__jindo2_callback._spellingCheck_0&q=%EC%95%88%EB%85%95%EB%94%94%EC%A7%80%EB%AA%AC"
-    prams = {"_callback":"window.__jindo2_callback._spellingCheck_0","q":text}
-    res = requests.get(url,params = params,headers=header)
+    url = "https://m.search.naver.com/p/csearch/ocontent/spellchecker.nhn"
+    params = {"_callback":"window.__jindo2_callback._spellingCheck_0","q":text}
+    res = requests.get(url,params=params,headers=header)
     textLength = len(res.text)
     jsonData = res.text[42:textLength-2]
     jsonData =json.loads(jsonData)
@@ -77,9 +78,9 @@ def make_reply(text):
     register_dic(words)
     for word in words:
         face = word[0]
-        if face in dic: make_sentence(face)
-
+        if face in dic: 
+            return make_sentence(face)
     return make_sentence("@")
 
 if os.path.exists(dic_file) :
-    dic = json.load(open(dic_file,'r'))
+    dic = json.load(open(dic_file,'r',encoding="utf-8"))
---
+++ test.py
@@ -0,0 +1,4 @@
+import json
+
+result = json.load(open('chat-data.json','r',encoding="utf-8"))
+print(result)
